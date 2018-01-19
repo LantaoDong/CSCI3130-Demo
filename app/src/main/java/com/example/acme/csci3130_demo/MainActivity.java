47a2ec55b0f6d7fe134f74ed57471d209b9a7659
lantaodong <championruss@163.com>
2018-01-18 22:49:31 -0400

    First assignment done!

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private EditText text;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+        text = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        textView.setText(text.getText());
     }
 }
